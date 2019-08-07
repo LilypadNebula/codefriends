a50b999b24d608abee122e5581a932518eafcc27
Lily <lilypadnebula@gmail.com>
2019-08-06 22:18:12 -0400

    Set up tailwind, purgecss, base of search

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col items-center p-32">
+    <input type="text" class="text-2xl p-4 border border-black rounded-lg" v-model="searchQuery" />
+    <button class="text-xl p-4 rounded bg-orange-400" @click="searchUsers">Search!</button>
+    <div v-if="loading">Loading...</div>
+    <div class="h-64 overflow-auto" v-else>
+      <p
+        class="text-xl p-2 border-b border-black"
+        v-for="person in people"
+        :key="person.id"
+      >{{person.login}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: () => ({
+    searchQuery: "",
+    loading: false,
+    people: []
+  }),
+  methods: {
+    async searchUsers() {
+      this.loading = true;
+      try {
+        let { data } = await axios.get(
+          `https://api.github.com/search/users?q=${this.searchQuery}`
+        );
+        this.people = data.items;
+      } catch (err) {
+        console.log(err);
+      } finally {
+        this.loading = false;
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="postcss">
+@tailwind base;
+
+@tailwind components;
+
+@tailwind utilities;
 </style>
