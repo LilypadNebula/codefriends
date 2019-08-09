8752a1186386e54218cb8182cebf1e8f6f62d945
Lily <lilypadnebula@gmail.com>
2019-08-09 16:55:28 -0400

    Fix postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,8 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Specify the paths to all of the template files in your project 
   content: [
-    './src/**/*.html',
+    'public/index.html',
     './src/**/*.vue',
-    './src/**/*.jsx',
     // etc.
   ],
 
